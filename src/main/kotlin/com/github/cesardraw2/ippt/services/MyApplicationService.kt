1a006d22e98334bce8e64917f23c637a67f2bdcd
GitHub Action <action@github.com>
2022-02-12 03:54:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/cesardraw2/ippt/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cesardraw2/ippt/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cesardraw2.ippt.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cesardraw2.ippt.MyBundle
 
 class MyApplicationService {
 
